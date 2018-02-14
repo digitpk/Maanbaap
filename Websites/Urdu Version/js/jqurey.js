0f327c48af4d98b71dd5c15b7f9c3a1c37dc383d
DIGIT Pakistan <digitpk@github.com>
2018-02-14 23:29:48 +0500

    Its the entrepenural website dealing with pregnancy issues in Pakistan

---
+++ Websites/Urdu Version/js/jqurey.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+	$(".container-fluid").css("display","none");
+	$(".container-fluid").slideDown(6000);
+	$(".upp-banner").css("display","none");
+	$(".upp-banner").slideDown(7000);
+
+});
